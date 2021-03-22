23e9fcb637873cc20932d48e8cbc56e19c57b63b
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2021-03-21 22:04:14 -0500

    change(env): Reconfigure Firebase project

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
 const functions = require('firebase-functions');
 const app = require('../src/backend/server');
 
-exports.app = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(app);
