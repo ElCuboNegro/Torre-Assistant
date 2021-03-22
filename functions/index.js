1df1ff1521721ae12f5eb5ab53109878cb821a0c
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2021-03-21 22:14:30 -0500

    change(@torre-assistant/backend): Move functions home to this module

--- functions/index.js
+++
@@ -1,4 +0,0 @@
-const functions = require('firebase-functions');
-const app = require('../src/backend/server');
-
-exports.api = functions.https.onRequest(app);
--- src/backend/index.js
+++
@@ -1,4 +0,0 @@
-const { environment } = require('./lib/config/config');
-const app = require('./server');
-
-app.listen(environment.PORT);
