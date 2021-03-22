b8a87e374909dd1b43e1ee0f2b5732d70cd2d877
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2021-03-21 20:33:02 -0500

    Remove unused sources
    Co-authored-by: juan jose alban ortiz <j.alban@uniandes.edu.co>

--- sources/backend/src/routes/user.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const express = require('express');
-const router = express.Router();
-const UserController = require.main.require('./controllers/user')
-router.get('/me', UserController.me);
-module.exports = router;
